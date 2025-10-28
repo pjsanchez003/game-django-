bcac256d04b24e4ed793f5512fc0f47fcc4c3fad
Paulina Jaqueline S. Álavrez <pj.sanchez003@ebc.edu.mx>
2025-10-27 20:03:32 -0600

    Arreglar URLs en base.html usando namespace juegos

--- juegos/urls.py
+++ juegos/urls.py
@@ -1,8 +1,12 @@
+# juegos/urls.py
 from django.urls import path
 from . import views
 
+app_name = "juegos"  
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('crear/', views.crear_juego, name='crear_juego'),
     path('editar/<int:id>/', views.editar_juego, name='editar_juego'),
     path('eliminar/<int:id>/', views.eliminar_juego, name='eliminar_juego'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("juegos.urls")),  # 👈 CAMBIA 'games' por 'juegos'
+    path("", include("juegos.urls", namespace="juegos")), 
 ]
 
 if settings.DEBUG:
